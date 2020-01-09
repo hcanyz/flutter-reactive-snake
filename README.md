c0088a2c7456d9e8a8576f64a54fdbf9da01c61e
yan.kong <hcanyz@163.com>
2020-01-09 20:48:48 +0800

    [init]

---
+++ README.md
@@ -0,0 +1,4 @@
+flutter-reactive-snake
+===
+
+> flutter rxdart 响应式 贪吃蛇
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(body: SafeArea(child: Text("flutter-snake"))),
+    );
+  }
+}
