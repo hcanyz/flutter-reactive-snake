1956bc1d6fa71705b039da92c3760ce45d2c6479
yan.kong <hcanyz@163.com>
2020-01-17 17:56:05 +0800

    [future] _direction$  _snack$

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,2 +1,11 @@
+import 'dart:math';
+
 //🐍状态刷新间隔
 const int TICKS_TICKS = 200;
+
+//🐍的初始状态
+const List<Point> SNACK_INIT = [Point(0, 0)];
+
+//游戏界面宽高
+const int GAME_WIDTH = 300;
+const int GAME_HEIGHT = 300;
--- lib/game.dart
+++ lib/game.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:collection';
+import 'dart:math';
 
 import 'package:flutter/services.dart';
 import 'package:rxdart/rxdart.dart';
@@ -6,34 +8,51 @@ import 'package:rxdart/rxdart.dart';
 import 'constants.dart';
 
 class Game {
-  Stream<LogicalKeyboardKey> _keyDown$;
-  Stream<int> _ticks$;
+  final StreamController<LogicalKeyboardKey> keyDownController =
+      StreamController<LogicalKeyboardKey>();
 
-  StreamController<LogicalKeyboardKey> keyDownController;
+  Stream<Scene> createGame() {
+    //数据更新频率流
+    Stream<int> _ticks$ = Stream.periodic(Duration(milliseconds: TICKS_TICKS),
+        (computationCount) => computationCount);
 
-  Game() {
-    keyDownController = _keyDown();
-    _keyDown$ = keyDownController.stream;
-    _ticks$ = _ticks();
-  }
+    //输入按键事件流,根据按键返回一个x,y方向变化值,过滤了不无意义按键，过滤了与当前方向相同的按键
+    Stream<Map<String, int>> _direction$ = keyDownController.stream.map((key) {
+      if (key == LogicalKeyboardKey.arrowUp) {
+        return const {"x": 0, "y": -1};
+      } else if (key == LogicalKeyboardKey.arrowDown) {
+        return const {"x": 0, "y": 1};
+      } else if (key == LogicalKeyboardKey.arrowLeft) {
+        return const {"x": -1, "y": 0};
+      } else if (key == LogicalKeyboardKey.arrowRight) {
+        return const {"x": 1, "y": 0};
+      }
+      return null;
+    }).where((change) {
+      return change != null;
+    }).distinct();
 
-  Stream<LogicalKeyboardKey> createGame() {
-    return _ticks$.withLatestFrom(_keyDown$, (_, LogicalKeyboardKey key) {
-      return key;
+    //🐍的状态 = 当前位置 + 方向 随时间更新
+    Stream<Queue<Point>> _snack$ =
+        _ticks$.withLatestFrom(_direction$, (_, Map<String, int> change) {
+      //每隔一段时间产生一个方向移动事件
+      print(change);
+      return change;
+    }).scan((Queue<Point> snack, Map<String, int> direction, index) {
+      //根据移动事件和当前位置，返回变化后的位置
+      return snack;
+    }, ListQueue.from(SNACK_INIT));
+
+    return _snack$.map((event) {
+      return Scene();
     });
   }
 
   void dispose() {
     keyDownController.close();
   }
+}
 
-  static Stream<int> _ticks() {
-    return Stream.periodic(Duration(milliseconds: TICKS_TICKS),
-        (computationCount) => computationCount);
-  }
-
-  static StreamController<LogicalKeyboardKey> _keyDown() {
-    var controller = StreamController<LogicalKeyboardKey>();
-    return controller;
-  }
+class Scene {
+  Scene() {}
 }
